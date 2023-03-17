331b85af9c146730ba24b6871a147329c82691a2
Tyler G. Hudson <tyler.g.hudson@gmail.com>
2023-03-17 09:06:17 -0700

    Update conftest.py
    Added comments to explain the purpose of the lines in pytest_sessionstart

--- test/conftest.py
+++ test/conftest.py
@@ -25,5 +25,12 @@ def image_id():
 
 
 def pytest_sessionstart(session):
+
+    # Navigate to the test folder path in order to make use of tests that require for
+    # a dockerfile to be in the local directory, regardless of where tests are being
+    # run from.
     os.chdir(Path(__file__).parent)
+
+    # Set the system to look for files in the repository root path so it can see the
+    # docker_cli module without having to install it.
     sys.path.insert(0, str(Path(__file__).parents[1]))
