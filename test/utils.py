99ca908cc0f97eedd444a7b155fa66f5b5c45020
Tyler G. Hudson <tyler.g.hudson@gmail.com>
2023-04-11 16:50:00 -0700

    Added some documentation to determine_scope

--- test/utils.py
+++ test/utils.py
@@ -6,6 +6,11 @@ def determine_scope(fixture_name, config) -> str:
     """
     Sets the scope of certain fixtures.
 
+    Currently always returns "function." This is because bugs appear if different scopes
+    are used. However, in the ideal case, parallel tests would have "function" scope
+    while serial tests would have a wider scope like "session" to avoid re-creating
+    identical docker images over and over.
+
     Parameters
     ----------
     fixture_name
@@ -18,6 +23,9 @@ def determine_scope(fixture_name, config) -> str:
     str
         The scope name.
     """
+    # XXX: This uses "function" scope at all times but serial tests would theoretically
+    # run faster if they had a wider scope. Presently this is necessary to prevent
+    # errors but the whole system would be faster in serial if "session" was used.
     if hasattr(config.option, "numprocesses"):
         if config.option.numprocesses in ["auto", "logical"]:
             return "function"
