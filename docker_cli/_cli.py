aa93bc16ace19abb523ca0faccaaaa22aecb7cef
tyler-g-hudson <tyler.g.hudson@jpl.nasa.gov>
2023-06-14 20:55:38 +0000

    Set the module location to be one file deeper

--- docker_cli/_cli.py
+++ docker_cli/_cli.py
@@ -17,7 +17,7 @@ from .setup_commands import (
 )
 
 # Acquire the location of defaults relative to the module
-module_location: Path = Path(__file__).parent
+module_location: Path = Path(__file__).parent.parent
 default_run_reqs_file: Path = module_location / "env_files" / "lock-runtime.txt"
 default_dev_reqs_file: Path = module_location / "env_files" / "lock-dev.txt"
 run_reqs_path_str = str(default_run_reqs_file)
