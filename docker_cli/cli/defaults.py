4924f1135dd586ab835b8669cc9dea0700f93dba
tyler-g-hudson <tyler.g.hudson@jpl.nasa.gov>
2023-07-26 21:52:15 +0000

    defaults.py PR fixes

--- docker_cli/cli/defaults.py
+++ docker_cli/cli/defaults.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 
 # Acquire the location of defaults relative to the module
-module_location: Path = Path(__file__).parent.parent.parent
+module_location: Path = Path(__file__).parents[2]
 default_run_reqs_file: Path = module_location / "env_files" / "lock-runtime.txt"
 default_dev_reqs_file: Path = module_location / "env_files" / "lock-dev.txt"
-run_reqs_path_str = str(default_run_reqs_file)
-dev_reqs_path_str = str(default_dev_reqs_file)
